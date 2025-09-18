b0904bf79f96ddd5e43a963d96f19fd4036abeed
DeepPurp13 <59247947+DeepPurp13@users.noreply.github.com>
2025-09-18 17:54:47 -0500

    happy bady upgrades

---
+++ src/components/LinkTreeLinks.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./LinkTreeLinks.css"; // create this file for styling
+
+const LinkTreeLinks = ({ links }) => {
+  return (
+    <div className="linktree-links">
+      {links.map((link, idx) => (
+        <a
+          key={idx}
+          href={link.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-button"
+        >
+          {link.title}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default LinkTreeLinks;
