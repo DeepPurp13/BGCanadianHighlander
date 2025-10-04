ebd9001a3b25d689d4b1aba8b6d9e589b1f14a21
DeepPurp13 <59247947+DeepPurp13@users.noreply.github.com>
2025-10-04 17:07:44 -0500

    link updates

--- src/LinkTree.js
+++ src/LinkTree.js
@@ -1,11 +1,12 @@
 import React from "react"; 
 import "./LinkTree.css"; 
 import LinkTreeLinks from "./components/LinkTreeLinks"; 
-  const links = [ { title: "Discord", url: "https://discord.gg/NMy3SgPFMP" }, 
-    { title: "What is Canadian Highlander?", url: "https://docs.google.com/document/d/1Q95RxteBrm68zukn0KpmSy4jWGlQlKjiEL38EZHXcX0/edit?tab=t.0" }, 
-    { title: "Local Meta Decklist", url: "https://docs.google.com/spreadsheets/d/116EWKLkp7o4FYEnSebEKfg_hQ7m4SOzv8GsSQG0ZdfA/edit?gid=0#gid=0" }, 
-    { title: "2025 New Player Primer", url: "https://canadianhighlander.ca/2025/01/06/2025-new-player-primer" }, 
-    { title: "Points List", url: "https://canadianhighlander.ca/points-list"} 
+  const links = [ 
+    { title: "Decklist", url: "https://moxfield.com/users/CanlanderDatabase" }, 
+    { title: "Points List", url: "https://canadianhighlander.ca/points-list/" }, 
+    { title: "New Player Primer", url: "https://canadianhighlander.ca/2025/01/06/2025-new-player-primer/" }, 
+    { title: "Proxies", url: "https://mtgprint.net/" }, 
+    { title: "Content", url: "https://www.youtube.com/playlist?list=PLzRiDUMuwrleJLb_yZbFk0XxDJZLcyOQ3"} 
   ];
   
   export default function LinkTree() 
@@ -15,3 +16,5 @@ import LinkTreeLinks from "./components/LinkTreeLinks";
        <LinkTreeLinks links={links} /> 
      </div> ); 
   }
+
+    
