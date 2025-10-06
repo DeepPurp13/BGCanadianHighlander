0223f6810a4953bf5ddf2300527a3dd5b9ce9861
DeepPurp13 <59247947+DeepPurp13@users.noreply.github.com>
2025-10-05 22:45:34 -0500

    our discord

--- src/LinkTree.js
+++ src/LinkTree.js
@@ -6,7 +6,8 @@ import LinkTreeLinks from "./components/LinkTreeLinks";
     { title: "Points List", url: "https://canadianhighlander.ca/points-list/" }, 
     { title: "New Player Primer", url: "https://canadianhighlander.ca/2025/01/06/2025-new-player-primer/" }, 
     { title: "Proxies", url: "https://mtgprint.net/" }, 
-    { title: "Content", url: "https://www.youtube.com/playlist?list=PLzRiDUMuwrleJLb_yZbFk0XxDJZLcyOQ3"} 
+    { title: "Content", url: "https://www.youtube.com/playlist?list=PLzRiDUMuwrleJLb_yZbFk0XxDJZLcyOQ3"},
+    { title: "Our Discord", url:"https://discord.gg/ejgKwxha"}
   ];
   
   export default function LinkTree() 
